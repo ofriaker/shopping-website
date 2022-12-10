670c541a62ea0c39825b3e9bffdee9ad7a338bab
ofriaker <ofriaker@gmail.com>
2022-12-10 19:03:23 +0200

    added styling

--- front/src/components/Product.js
+++ front/src/components/Product.js
@@ -4,16 +4,23 @@ import './Product.css';
 
 export default class Product extends Component {
   render() {
-  return (
-    <div className='product-item'>        
-        <div className='product-details'>
-          <p>Product name: {this.props.name}</p>
-          <p>Price: {this.props.price}</p>
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-8'>
+            <div className='mt-4 fs-4'>
+              <p>Product name: {this.props.name}</p>
+              <p>Price: {this.props.price}</p>
+              <button class="btn btn-outline-success" >Add To Shopping Cart</button>
+            </div>
+
+          </div>
+          <div className='col-sm-4'>
+            <img className='mw-100 h-80 rounded float-right' src={this.props.productImageUrl} alt="" />
+          </div>
         </div>
-      <div>
-        <img  className='product-image' src={this.props.productImageUrl} alt="" />        
+        <hr></hr>
       </div>
-    </div>
-  );
+    ); 
   }
 }
